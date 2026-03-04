03c7f11a0e0ef5b571c0b79bce17e6e4e4abe012
fahimx51 <fahim69.dev@gmail.com>
2026-03-05 03:37:50 +0600

    Added functions for multer

--- middleware/upload.js
+++ middleware/upload.js
@@ -2,5 +2,42 @@ const multer = require('multer');
 const fs = require('fs');
 
 const storage = multer.diskStorage({
+    destination: (req, file, cb) => {
+        const folder = file.fieldname === 'profilePic' ? 'uploads/profile' : 'uploads/messeges';
 
+        if (!fs.existsSync(folder)) {
+            fs.mkdirSync(folder, { recursive: true });
+        }
+
+        cb(null, folder);
+    },
+
+    filename: (req, file, cb) => {
+        cb(null, `${Date.now()} - ${file.originalname.replace(/\s/g, '-')}`);
+    }
 });
+
+const fileFilter = (req, file, cb) => {
+    const allowedTypes = [
+        'image/jpeg',
+        'image/jpg',
+        'image/png',
+        'image/gif',
+        'image/webp',
+        'application/pdf',
+    ];
+
+    if (allowedTypes.includes(file.mimetype)) {
+        cb(null, true);
+    } else {
+        cb(new Error(`Unsupported file type : ${file.mimetype}`));
+    }
+};
+
+const upload = multer({
+    storage,
+    fileFilter,
+    limits: { fileSize: 10 * 1024 * 1024 }
+});
+
+module.exports = upload;
--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -2,10 +2,17 @@ const express = require('express');
 const router = express.Router();
 const userController = require('../controllers/userController');
 const protector = require('../middleware/authMiddleware');
+const upload = require('../middleware/upload');
 
 
 router.get('/', protector, userController.getAllUser);
-router.put('/profile', protector, userController.updateProfile);
+
+router.put(
+    '/profile',
+    protector,
+    upload.single('profilePic'),
+    userController.updateProfile
+);
 
 
 module.exports = router;
